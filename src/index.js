0290db402aacafb50dbd4cd64390ce00663b5b57
Nishant Bhagat <82897281+Nishant-IIT@users.noreply.github.com>
2023-08-28 16:16:23 +0000

    Update index.js and mui.tsx

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import ValueGetterGrid from '/Users/nishant/Desktop/Tagic/vms/grid component/muigrid/my-app/src/mui.tsx';
+// import ValueGetterGrid from '/Users/nishant/Desktop/Tagic/vms/grid component/muigrid/my-app/src/mui.tsx';
 import App1 from './handsOnTable';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <ValueGetterGrid />
-    <App1 />fabClasses
+    {/* <ValueGetterGrid /> */}
+    <App1 />
   </React.StrictMode>
 );
